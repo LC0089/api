84d06149f2ece857b843b8141ab8ea7b8eba0543
LC0089 <1652324132@qq.com>
2019-05-05 03:24:07 +0000

    Api

---
+++ app/Http/Api/ApiController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+class ApiController extends Controller{
+
+}
